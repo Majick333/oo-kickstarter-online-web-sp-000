03cac2f35bd72a3ef8c5dcc93993135f9adc4978
Ryan M <rmccauleymusic@gmail.com>
2019-11-21 23:42:40 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+
+class Backer
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(Project)
+    @backed_projects << self.Project
+  end
+
+
+
+end
